940048f79b75509ff8005d67ca1b208d62bfef9b
Mark Ross-Lonergan <markrl@houston.nevis.columbia.edu>
2018-09-25 16:18:57 -0400

    Load root files on the fly in doMC and not before for memory issues

--- src/SBgeN.cxx
+++ src/SBgeN.cxx
@@ -5,23 +5,8 @@ using namespace sbn;
 SBgeN::SBgeN(std::string whichxml) : SBNspec(whichxml,-1) {
 
 	//Load all files as per xml
-	std::cout<<"SBgeN::SBgeN || Opening all files in xml: "<<whichxml<<std::endl;
-	for(auto &fn: multisim_file){
-		files.push_back(new TFile(fn.c_str()));
-		std::cout<<"SBgeN::SBgeN || Files: "<<fn<<std::endl;
-	}
-	Nfiles = files.size();
+	Nfiles = multisim_file.size();
 	std::cout<<"SBgeN::SBgeN || Loaded "<<Nfiles<<" in total (multisum_file.size()): "<<multisim_file.size()<<std::endl;
-
-	std::cout<<"SBgeN::SBgeN || Getting all TTrees from Files."<<std::endl;
-	for(int i=0; i<multisim_name.size(); i++){
-		trees.push_back((TTree*)files.at(i)->Get(multisim_name[i].c_str()) );
-	}
-
-	for(auto &t: trees){
-	//	nentries.push_back(100000);
-		nentries.push_back(t->GetEntries());
-	}
 
 
 	//load all doubles, and int.. as mentioned in xml
@@ -57,39 +42,6 @@ SBgeN::SBgeN(std::string whichxml) : SBNspec(whichxml,-1) {
 	}
 
 
-
-	std::cout<<"SBgeN::SBgeN || Starting Branch Address Assignment."<<std::endl;
-
-	for(int i=0; i< Nfiles; i++){
-			for(int k=0; k< branch_names_int.at(i).size();k++){
-				trees.at(i)->SetBranchAddress(branch_names_int.at(i).at(k).c_str(), &(vars_i.at(i).at(k)));
-				std::cout<<"SBgeN::SBgeN || Setting Integer Branch: "<<branch_names_int.at(i).at(k)<<std::endl;
-			}
-			for(int k=0; k< branch_names_int_array.at(i).size();k++){
-				trees.at(i)->SetBranchAddress( branch_names_int_array.at(i).at(k).c_str(), vars_iA.at(i).at(k).data  );
-				std::cout<<"SBgeN::SBgeN || Setting Integer Array Branch: "<<branch_names_int_array.at(i).at(k)<<std::endl;
-			}
-
-
-			for(int k=0; k< branch_names_double.at(i).size();k++){
-				trees.at(i)->SetBranchAddress(branch_names_double.at(i).at(k).c_str(), &(vars_d.at(i).at(k)));
-				std::cout<<"SBgeN::SBgeN || Setting Double Branch: "<<branch_names_double.at(i).at(k)<<std::endl;
-			}
-		int jj=0;
-			for(int k=0; k< branch_names_double_array.at(i).size();k++){
-				if( branch_names_double_array_dimension.at(jj).at(k) == 3){
-					trees.at(i)->SetBranchAddress(branch_names_double_array.at(i).at(k).c_str(), vars_dA.at(i).at(k).data3);
-					std::cout<<"SBgeN::SBgeN || Setting Double Array Branch Dim 3: "<<branch_names_double_array.at(i).at(k)<<std::endl;
-				}else{
-					trees.at(i)->SetBranchAddress(branch_names_double_array.at(i).at(k).c_str(), vars_dA.at(i).at(k).data);
-					std::cout<<"SBgeN::SBgeN || Setting Double Array Branch "<<branch_names_double_array.at(i).at(k)<<std::endl;
-				}
-
-				jj++;
-			}
-	}
-
-
 	std::cout<<"SBgeN::SBgeN || Setting up detectors SBNdet."<<std::endl;
 	for(int i=0; i< detector_names.size(); i++){
 		if(detector_bool[i]){
@@ -115,25 +67,61 @@ int SBgeN::doMC(std::string nam){
 
 	std::cout<<"SBgeN::doMC || We have "<<Nfiles<<" Files "<<std::endl;
 	for(int j=0;j<Nfiles;j++){
+
+		files.push_back(new TFile(multisim_file[j].c_str()));
+		trees.push_back((TTree*)files.back()->Get(multisim_name[j].c_str()) );
+		nentries.push_back(trees.back()->GetEntries());
+
+
+		std::cout<<"SBgeN::SBgeN || Starting Branch Address Assignment."<<std::endl;
+		for(int k=0; k< branch_names_int.at(j).size();k++){
+			trees.at(j)->SetBranchAddress(branch_names_int.at(j).at(k).c_str(), &(vars_i.at(j).at(k)));
+			std::cout<<"SBgeN::SBgeN || Setting Integer Branch: "<<branch_names_int.at(j).at(k)<<std::endl;
+		}
+		for(int k=0; k< branch_names_int_array.at(j).size();k++){
+			trees.at(j)->SetBranchAddress( branch_names_int_array.at(j).at(k).c_str(), vars_iA.at(j).at(k).data  );
+			std::cout<<"SBgeN::SBgeN || Setting Integer Array Branch: "<<branch_names_int_array.at(j).at(k)<<std::endl;
+		}
+
+
+		for(int k=0; k< branch_names_double.at(j).size();k++){
+			trees.at(j)->SetBranchAddress(branch_names_double.at(j).at(k).c_str(), &(vars_d.at(j).at(k)));
+			std::cout<<"SBgeN::SBgeN || Setting Double Branch: "<<branch_names_double.at(j).at(k)<<std::endl;
+		}
+		int jj=0;
+		for(int k=0; k< branch_names_double_array.at(j).size();k++){
+			if( branch_names_double_array_dimension.at(jj).at(k) == 3){
+				trees.at(j)->SetBranchAddress(branch_names_double_array.at(j).at(k).c_str(), vars_dA.at(j).at(k).data3);
+				std::cout<<"SBgeN::SBgeN || Setting Double Array Branch Dim 3: "<<branch_names_double_array.at(j).at(k)<<std::endl;
+			}else{
+				trees.at(j)->SetBranchAddress(branch_names_double_array.at(j).at(k).c_str(), vars_dA.at(j).at(k).data);
+				std::cout<<"SBgeN::SBgeN || Setting Double Array Branch "<<branch_names_double_array.at(j).at(k)<<std::endl;
+			}
+
+			jj++;
+		}
+
+
 		std::cout<<"SBgeN::doMC || Starting file and event loops. On File: "<<multisim_file.at(j)<<" || "<<multisim_name.at(j)<<" #eventts: "<<nentries.at(j)<<std::endl;
 		double pot_factor = 1;//pot.at(j)/(pot_scaling.at(j) * (double)nentries.at(j));
 
-		for(int i=0; i< nentries.at(j); i++){
-			//if(i%250000==0)std::cout<<"SBgeN::doMC || Event: "<<i<<" of "<<nentries.at(j)<<" POT factor: "<<pot_factor<<" on File "<<j<<std::endl;
+		for(int i=0; i< nentries.back(); i++){
+
 
 			trees.at(j)->GetEntry(i);
 			//here we put low level selection criteria, for example nuance interaction 1001 == CCQE, its a virtual bool.
-			{
-			//if( this->eventSelection(j) ){
-				//This is the part where we will every histogram in this Universe
-				this->fillHistograms(j, -1, 1);
-			}//end of low level selection
+			//if( this->eventSelection(j) ){}
+			//This is the part where we will every histogram in this Universe
+			this->fillHistograms(j, -1, 1);
 
 		}//end of entry loop	
 
 
-	double scaled_events = (hist.at(map_hist[multisim_name.at(j)])).GetSumOfWeights();
-	std::cout<<"SBgeN::doMC || on\t"<<multisim_name.at(j)<<"\t\tEntries:\t"<<nentries.at(j)<<" Scaled Events:\t"<<scaled_events<<" ratio to entries:\t"<<scaled_events/((double)nentries.at(j))<<"\tFor(x10):\t"<<(scaled_events/((double)nentries.at(j)))/0.1<<" ND: "<<(10*scaled_events/((double)nentries.at(j)))/0.1<<std::endl;
+		double scaled_events = (hist.at(map_hist[multisim_name.at(j)])).GetSumOfWeights();
+		std::cout<<"SBgeN::doMC || on\t"<<multisim_name.at(j)<<"\t\tEntries:\t"<<nentries.at(j)<<" Scaled Events:\t"<<scaled_events<<" ratio to entries:\t"<<scaled_events/((double)nentries.at(j))<<"\tFor(x10):\t"<<(scaled_events/((double)nentries.at(j)))/0.1<<" ND: "<<(10*scaled_events/((double)nentries.at(j)))/0.1<<std::endl;
+
+
+	files.back()->Close();
 
 	}//end of file loop 
 
@@ -159,13 +147,13 @@ int SBgeN::doMC(std::string nam){
 SBgeN::~SBgeN(){
 	std::cout<<"SBgeN::~SBgeN || Destructor Starting"<<std::endl;
 	for(auto &f: files){
-		f->Close();
-		delete f;
+	//	f->Close();
+	//	delete f;
 	}
 	for(auto &t: trees){
 		//delete t;
 	}
-	
+
 
 	std::cout<<"SBgeN::~SBgeN || Destructor Done"<<std::endl;
 }
